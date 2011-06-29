92fea6f23230176cfc26e674e898cece94b512c5
Eric Stein <toba@des.truct.org>
2011-06-29 00:47:18 -0500

    partially pull out the relevant data - not parsing the data points yet, a bit hacky

--- pywp.py
+++ pywp.py
@@ -1,3 +1,4 @@
+import pprint
 import urllib2
 import xml.parsers.expat
 
@@ -7,13 +8,50 @@ def predict(latitude, longitude) :
 	parse(wstream)
 	wstream.close()
 
+# TODO use time-layout to parse time zone correctly
+
 def parse(fh) :
+	class ParseState(object) :
+		pass
+
+	ps = ParseState()
+
+	ps.layouts = {}
+	ps.layout_key = None
+	ps.element = None
+	ps.traces = {}
+	ps.trace_key = None
+
+	useful_elements = [
+		'temperature',
+		'probability-of-precipitation',
+		'wind-speed',
+		'direction',
+		'cloud-amount',
+		'humidity',
+		'hourly-qpf'
+	]
+
 	def start_element(name, attrs):
-	    print 'Start element:', name, attrs
+		print 'DEBUG Start element:', name, attrs
+		ps.element = name
+		if name in useful_elements :
+			ps.trace_key = '%s.%s' % (name, attrs['type'])
+			ps.traces.setdefault(ps.trace_key, [])
+		if 'xsi:nil' in attrs and name == 'value' :
+			ps.traces[ps.trace_key].append(None)
 	def end_element(name):
-	    print 'End element:', name
+		print 'DEBUG End element:', name
+		ps.element = None
 	def char_data(data):
-	    print 'Character data:', repr(data)
+		print 'DEBUG Character data:', repr(data)
+		if ps.element == 'layout-key' :
+			ps.layout_key = data
+			ps.layouts[ps.layout_key] = []
+		if ps.element == 'start-valid-time' :
+			ps.layouts[ps.layout_key].append(data)
+		if ps.element == 'value' :
+			ps.traces[ps.trace_key].append(data)
 
 	p = xml.parsers.expat.ParserCreate()
 
@@ -22,6 +60,9 @@ def parse(fh) :
 	p.CharacterDataHandler = char_data
 
 	p.Parse(fh.read())
+
+	pprint.pprint(ps.layouts)
+	pprint.pprint(ps.traces)
 
 if __name__ == '__main__' :
 	predict(41.837, -87.685)
