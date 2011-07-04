ede7618d4a331dfb7fc1782f467e582444f37fa7
Eric Stein <toba@des.truct.org>
2011-07-04 17:53:16 -0500

    auto convert type of values, less debug noise

--- pywp.py
+++ pywp.py
@@ -21,6 +21,7 @@ def parse(fh) :
 	ps.element = None
 	ps.traces = {}
 	ps.trace_key = None
+	ps.converter = None
 
 	useful_elements = [
 		'temperature',
@@ -32,26 +33,38 @@ def parse(fh) :
 		'hourly-qpf'
 	]
 
+	def debug(s) :
+		pass#print 'DEBUG %s' % s
+
 	def start_element(name, attrs):
-		print 'DEBUG Start element:', name, attrs
+		debug('Start element: %s %s' % (name, attrs))
 		ps.element = name
 		if name in useful_elements :
 			ps.trace_key = '%s.%s' % (name, attrs['type'])
+			if (attrs['type'] == 'floating') :
+				ps.converter = float
+			else :
+				ps.converter = int
 			ps.traces.setdefault(ps.trace_key, [])
 		if 'xsi:nil' in attrs and name == 'value' :
 			ps.traces[ps.trace_key].append(None)
 	def end_element(name):
-		print 'DEBUG End element:', name
+		debug('End element: %s' % name)
 		ps.element = None
 	def char_data(data):
-		print 'DEBUG Character data:', repr(data)
+		debug('Character data: %s' % repr(data))
 		if ps.element == 'layout-key' :
 			ps.layout_key = data
 			ps.layouts[ps.layout_key] = []
 		if ps.element == 'start-valid-time' :
 			ps.layouts[ps.layout_key].append(data)
 		if ps.element == 'value' :
-			ps.traces[ps.trace_key].append(data)
+			value = data
+			try :
+				value = ps.converter(data)
+			except ValueError :
+				pass # TODO handle this better?
+			ps.traces[ps.trace_key].append(value)
 
 	p = xml.parsers.expat.ParserCreate()
 
